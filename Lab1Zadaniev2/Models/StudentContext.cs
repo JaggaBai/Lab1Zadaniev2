138dbac16b1fc9aeeb3c594b2ea53311f5278d1e
Jagga Bai <dzagur@post.pl>
2023-07-07 16:05:36 +0200

    Dodaj pliki projektów.

---
+++ Lab1Zadaniev2/Models/StudentContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab1Zadaniev2.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; } = null!;
+    }
+}
